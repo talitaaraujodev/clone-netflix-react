5fd35952d7e8244371f06538f4948ff1344a6eab
talitaaraujodev <talitacumi.araujo@gmail.com>
2022-08-02 21:17:38 -0300

    feat: clone netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./assets/css/App.css";
+import { FeaturedMovie } from "./components/FeaturedMovie";
+import { Header } from "./components/Header";
+import { MovieRow } from "./components/MovieRow";
+import { Api } from "./services/Api";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  useEffect(() => {
+    const loadAll = async () => {
+      let data = await Api.getHomeList();
+      setMovieList(data);
+      //destaque
+      let originais = data.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originais[0].items.results.length - 1)
+      );
+      let chosen = originais[0].items.results[randomChosen];
+      let chosenInfo = await Api.getMovieInfo(chosen.id, "tv");
+      console.log(chosenInfo);
+      setFeatureData(chosenInfo);
+    };
+    loadAll();
+  }, []);
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header black={blackHeader} />
+      {featureData && <FeaturedMovie item={featureData} />}
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+      <footer>
+        Feito com{" "}
+        <span role={"img"} aria-label="Coração">
+          🥰
+        </span>{" "}
+        por Talita Araújo
+        <br />
+        Direitos de imagem para Netflix Dados oriundos do site Themoviedb.org
+      </footer>
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://c.tenor.com/DQyztbEmqnYAAAAC/netflix-loading.gif"
+            alt="Loader Netflix"
+          />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import "./Header.css";
+export function Header({ black }) {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Logo Netflix"
+          />
+        </a>
+      </div>
+      <div className="header-user">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Usuário"
+          />
+        </a>
+      </div>
+    </header>
+  );
+}
